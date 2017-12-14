b75e580383c56b2aed76436cd673d1557ec9af58
satyam <invinciblesatyam@gmail.com>
2017-12-14 15:06:34 +0530

    finished adding basic layout

--- src/ClientGui.java
+++ src/ClientGui.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
@@ -10,8 +11,12 @@ class ClientGui
 	private JFrame frame;
 	private ClientPanel mpanel; //the main content panel
 	private JPanel hpanel;  //where all the messages are displayed
-	private JPanel msgpanel; //where message is tyed
+	private JPanel hpanelback;
+	private JPanel msgpanel;  //where message is typed
+	private JPanel menupanel;
+	private JPanel friendspanel;
 	private JScrollPane scroller;
+	private JPanel trash;
 	//private JTextArea text;
 	private JTextArea message;
 	private JButton button;
@@ -22,15 +27,13 @@ class ClientGui
 	public static void main(String args[])
 	{
 		ClientGui cl=new ClientGui();
-		Thread th=new Thread(new Runnable(){
-			public void run()
+		Thread th=new Thread(()->
 			{
 				while(true)
 				{
 					cl.chat();
 				}
-			}
-		});
+			});
 		th.start();
 	}
 
@@ -42,10 +45,31 @@ class ClientGui
 		
 		mpanel=new ClientPanel();
 		mpanel.setSize(1300,700);
-		mpanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-		//mpanel.setOpaque(false);
+		mpanel.setLayout(new BorderLayout());
 		frame.setContentPane(mpanel);
-		
+
+		menupanel=new JPanel();
+		menupanel.setPreferredSize(new Dimension(1300,30));
+		menupanel.setOpaque(false);
+		mpanel.add(menupanel,BorderLayout.NORTH);
+
+		trash=new JPanel();
+		trash.setPreferredSize(new Dimension(10,600));
+		trash.setOpaque(false);
+		mpanel.add(trash,BorderLayout.WEST);
+
+		hpanelback=new JPanel()
+		{
+			public void paintComponent(Graphics g)
+			{
+				Image image=new ImageIcon("/home/satyamholmes/Desktop/java/Lan_Chat/hpanel.png").getImage();
+				g.drawImage(image,0,0,1005,600,this);
+			}
+		};
+		hpanelback.setLayout(new FlowLayout(FlowLayout.LEFT));
+		hpanelback.setOpaque(false);
+		hpanelback.setPreferredSize(new Dimension(1005,600));
+
 		hpanel=new JPanel();
 		hpanel.setLayout(new BoxLayout(hpanel,BoxLayout.Y_AXIS));
 		hpanel.setOpaque(false);
@@ -53,32 +77,31 @@ class ClientGui
 		scroller=new JScrollPane(hpanel);
 		scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 		scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scroller.setPreferredSize(new Dimension(1200,600));
+		scroller.setPreferredSize(new Dimension(1000,580));
 		scroller.getViewport().setOpaque(false);
 		scroller.setOpaque(false);
 		scroller.getVerticalScrollBar().setUI(new MyScrollBar());
 		scroller.getVerticalScrollBar().setOpaque(false);
 		scroller.setBorder(BorderFactory.createEmptyBorder());
 		scroller.setViewportBorder(BorderFactory.createEmptyBorder());
-
-		mpanel.add(scroller);
-
 		scroller.getVerticalScrollBar().addAdjustmentListener((evb) -> {
 			hpanel.repaint();
 		});
 		scroller.getVerticalScrollBar().setUnitIncrement(10);
 
+		hpanelback.add(scroller);
+		mpanel.add(hpanelback,BorderLayout.CENTER);
+
 		msgpanel=new JPanel();
 		msgpanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 		msgpanel.setPreferredSize(new Dimension(1300,50));
-		mpanel.add(msgpanel);
 		msgpanel.setOpaque(false);
-		
+		mpanel.add(msgpanel,BorderLayout.SOUTH);
+
 		message=new JTextArea();
 		msgpanel.add(message);
 		message.setPreferredSize(new Dimension(1100,50));
 		message.setLineWrap(true);
-		
 
 		message.addKeyListener(new KeyAdapter()
 			{
@@ -134,6 +157,15 @@ class ClientGui
 						ex.printStackTrace();
 					}
 				});
+
+		friendspanel=new JPanel()
+		{
+			public void paintComponent(Graphics g)
+			{
+				Image image=new ImageIcon("/home/satyamholmes/Desktop/java/Lan_Chat/hpanel.png").getImage();
+				g.drawImage(image,0,0,300,600,this);
+			}
+		};
 		frame.setVisible(true);
 		connect();
 	}
@@ -168,13 +200,14 @@ class ClientGui
 			};
 			rpanel.setPreferredSize(new Dimension(1300,20));
 			rpanel.setMaximumSize(new Dimension(1300,20));
-			rpanel.setMinimumSize(new Dimension(1300,20));	
+			rpanel.setMinimumSize(new Dimension(1300,20));
 			hpanel.add(rpanel);
 			hpanel.revalidate();
 			hpanel.repaint();
 			javax.swing.Timer t=new javax.swing.Timer(0,(eb)->
-					scroller.getVerticalScrollBar().setValue(scroller.getVerticalScrollBar().getMaximum());
-				);
+			{
+				scroller.getVerticalScrollBar().setValue(scroller.getVerticalScrollBar().getMaximum());
+			});
 			t.setRepeats(false);
 			t.setInitialDelay(20);
 			t.start();
@@ -193,6 +226,5 @@ class ClientGui
 			g.drawImage(image,0,0,1300,700,this);
 		}
 	}
-
 }
 
